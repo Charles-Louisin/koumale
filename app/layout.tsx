da6c753116e26f6b1f14120999b19271f53486bc
Charles Louisin <clynlouisin@gmail.com>
2025-12-01 10:24:31 +0100

    indexation pousse

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "KOUMALE - Marketplace multi-vendeur",
-  description: "Marketplace centralisé",
+  description: "Plateforme marketplace centralisée pour petits et grands commerces.",
 };
 
 export default function RootLayout({
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         {/* <meta name="google-site-verification" content="PdDCFV8PLsziDmPA1Yx0Por5sJYjFAXOIni9VnR3dxk" /> */}
+        <link rel="canonical" href="https://koumale.com" />
         <meta name="google-site-verification" content="ml4bJHmhlGdRmam6iocKk_jo3nvI6Kx1mJ6zDGYv1zM" />
       </head>
       <body
